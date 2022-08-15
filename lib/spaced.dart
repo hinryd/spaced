bdd8691c6211153e8a2952e5b9baf92560280ae1
Henry <lkhh@protonmail.com>
2022-08-15 13:42:54 +0800

    Add SpacedFlex widget and fix mainAxisAlignment

--- lib/spaced.dart
+++ lib/spaced.dart
@@ -2,6 +2,43 @@ library spaced;
 
 import 'package:flutter/widgets.dart';
 
+/// Functionally same as [Flex] but with [SizedBox] in between all the children.
+///
+/// [spacing] will only apply when [MainAxisAlignment] is [MainAxisAlignment.start], [MainAxisAlignment.center] or [MainAxisAlignment.end].
+///
+/// If you need a scrollable list, consider using [ListView.separated] instead.
+class SpacedFlex extends Flex {
+  SpacedFlex({
+    required this.spacing,
+    List<Widget> children = const <Widget>[],
+    required super.direction,
+    super.mainAxisAlignment = MainAxisAlignment.start,
+    super.key,
+    super.mainAxisSize = MainAxisSize.max,
+    super.crossAxisAlignment = CrossAxisAlignment.center,
+    super.textDirection,
+    super.verticalDirection = VerticalDirection.down,
+    super.textBaseline,
+    super.clipBehavior = Clip.none,
+  }) : super(
+            children: mainAxisAlignment == MainAxisAlignment.start ||
+                    mainAxisAlignment == MainAxisAlignment.center ||
+                    mainAxisAlignment == MainAxisAlignment.end ||
+                    !(spacing <= 0)
+                ? (children
+                    .expand((child) => [
+                          child,
+                          direction == Axis.horizontal
+                              ? SizedBox(width: spacing)
+                              : SizedBox(height: spacing)
+                        ])
+                    .toList()
+                  ..removeLast())
+                : children);
+
+  final double spacing;
+}
+
 /// Functionally same as [Column] but with [SizedBox] in between all the children.
 ///
 /// [spacing] will only apply when [MainAxisAlignment] is [MainAxisAlignment.start], [MainAxisAlignment.center] or [MainAxisAlignment.end].
@@ -11,7 +48,7 @@ class SpacedColumn extends Column {
   SpacedColumn({
     required this.spacing,
     List<Widget> children = const <Widget>[],
-    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    super.mainAxisAlignment = MainAxisAlignment.start,
     super.key,
     super.crossAxisAlignment,
     super.mainAxisSize,
@@ -42,7 +79,7 @@ class SpacedRow extends Row {
   SpacedRow({
     required this.spacing,
     List<Widget> children = const <Widget>[],
-    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    super.mainAxisAlignment = MainAxisAlignment.start,
     super.key,
     super.crossAxisAlignment,
     super.mainAxisSize,
